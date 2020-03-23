1e8f0b197402e19c716ce98b4570937892fdba2e
austin-stocks <sundeep_chadha@yahoo.com>
2020-03-22 22:07:51 -0500

    Initial version

--- git_commit.sh
+++ git_commit.sh
@@ -16,16 +16,16 @@ git add [YZ]*jpg
 echo "Now starting the commit provess"
 
 echo "Commiting files that start with [ABCD]..."
-git commit -m "More updates"  [ABCD]*jpg
+git commit -m "More updates"  [ABCD]*jpg -v
 echo "Commiting files that start with [EFGH]..."
-git commit -m "More updates"  [EFGH]*jpg
+git commit -m "More updates"  [EFGH]*jpg -v
 echo "Commiting files that start with [IJKL]..."
-git commit -m "More updates"  [IJKL]*jpg
+git commit -m "More updates"  [IJKL]*jpg -v
 echo "Commiting files that start with [MNOP]..."
-git commit -m "More updates"  [MNOP]*jpg
+git commit -m "More updates"  [MNOP]*jpg -v
 echo "Commiting files that start with [QRST]..."
-git commit -m "More updates"  [QRST]*jpg
+git commit -m "More updates"  [QRST]*jpg -v
 echo "Commiting files that start with [UVWX]..."
-git commit -m "More updates"  [UVWX]*jpg
+git commit -m "More updates"  [UVWX]*jpg -v
 echo "Commiting files that start with [YZ]..."
-git commit -m "More updates"  [YZ]*jpg
+git commit -m "More updates"  [YZ]*jpg -v
