d1ce8d649bcdd04a9133a7a6845a041796c23b05
austin-stocks <sundeep_chadha@yahoo.com>
2020-03-22 22:06:58 -0500

    Initial version

---
+++ git_commit.sh
@@ -0,0 +1,31 @@
+echo "Addting files that start with [ABCD]..."
+git add [ABCD]*jpg
+echo "Addting files that start with [EFGH]..."
+git add [EFGH]*jpg
+echo "Addting files that start with [IJKL]..."
+git add [IJKL]*jpg
+echo "Addting files that start with [MNOP]..."
+git add [MNOP]*jpg
+echo "Addting files that start with [QRST]..."
+git add [QRST]*jpg
+echo "Addting files that start with [UVWX]..."
+git add [UVWX]*jpg
+echo "Addting files that start with [YZ]..."
+git add [YZ]*jpg
+
+echo "Now starting the commit provess"
+
+echo "Commiting files that start with [ABCD]..."
+git commit -m "More updates"  [ABCD]*jpg
+echo "Commiting files that start with [EFGH]..."
+git commit -m "More updates"  [EFGH]*jpg
+echo "Commiting files that start with [IJKL]..."
+git commit -m "More updates"  [IJKL]*jpg
+echo "Commiting files that start with [MNOP]..."
+git commit -m "More updates"  [MNOP]*jpg
+echo "Commiting files that start with [QRST]..."
+git commit -m "More updates"  [QRST]*jpg
+echo "Commiting files that start with [UVWX]..."
+git commit -m "More updates"  [UVWX]*jpg
+echo "Commiting files that start with [YZ]..."
+git commit -m "More updates"  [YZ]*jpg
