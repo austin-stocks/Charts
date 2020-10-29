02cfdb65b7ea914dd5a0cb1bb3c887e20227ab36
austin-stocks <sundeep_chadha@yahoo.com>
2020-10-29 14:24:08 -0500

    More updates

--- git_commit.sh
+++ git_commit.sh
@@ -7,81 +7,26 @@ echo ""
 echo "=====> Now starting the add process <====="
 echo ""
 
-echo "Adding files that start with [A]..."
-git  add Linear/Charts_With_Numbers/[A]*jpg -v
-git  add Linear/Charts_Without_Numbers/[A]*jpg -v
-echo "Adding files that start with [B]..."
-git  add Linear/Charts_With_Numbers/[B]*jpg -v
-git  add Linear/Charts_Without_Numbers/[B]*jpg -v
-echo "Adding files that start with [C]..."
-git  add Linear/Charts_With_Numbers/[C]*jpg -v
-git  add Linear/Charts_Without_Numbers/[C]*jpg -v
-echo "Adding files that start with [D]..."
-git  add Linear/Charts_With_Numbers/[D]*jpg -v
-git  add Linear/Charts_Without_Numbers/[D]*jpg -v
-echo "Adding files that start with [EFGH]..."
-git  add Linear/Charts_With_Numbers/[EFGH]*jpg -v
-git  add Linear/Charts_Without_Numbers/[EFGH]*jpg -v
-echo "Adding files that start with [IJKL]..."
-git  add Linear/Charts_With_Numbers/[IJKL]*jpg -v
-git  add Linear/Charts_Without_Numbers/[IJKL]*jpg -v
-echo "Adding files that start with [MN]..."
-git  add Linear/Charts_With_Numbers/[MN]*jpg -v
-git  add Linear/Charts_Without_Numbers/[MN]*jpg -v
-echo "Adding files that start with [OP]..."
-git  add Linear/Charts_With_Numbers/[OP]*jpg -v
-git  add Linear/Charts_Without_Numbers/[OP]*jpg -v
-echo "Adding files that start with [QR]..."
-git  add Linear/Charts_With_Numbers/[QR]*jpg -v
-git  add Linear/Charts_Without_Numbers/[QR]*jpg -v
-echo "Adding files that start with [ST]..."
-git  add Linear/Charts_With_Numbers/[ST]*jpg -v
-git  add Linear/Charts_Without_Numbers/[ST]*jpg -v
-echo "Adding files that start with [UVWX]..."
-git  add Linear/Charts_With_Numbers/[UVWX]*jpg -v
-git  add Linear/Charts_Without_Numbers/[UVWX]*jpg -v
-echo "Adding files that start with [YZ]..."
-git  add Linear/Charts_With_Numbers/[YZ]*jpg -v
-git  add Linear/Charts_Without_Numbers/[YZ]*jpg -v
 
-echo ""
-echo "=====> Now starting the commit process <====="
-sleep 2
-echo ""
+alphabets='A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
 
-echo "Commiting files that start with [AB]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[A]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[A]*jpg -v
-echo "Commiting files that start with [B]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[B]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[B]*jpg -v
-echo "Commiting files that start with [C]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[C]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[C]*jpg -v
-echo "Commiting files that start with [D]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[D]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[D]*jpg -v
-echo "Commiting files that start with [EFGH]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[EFGH]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[EFGH]*jpg -v
-echo "Commiting files that start with [IJKL]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[IJKL]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[IJKL]*jpg -v
-echo "Commiting files that start with [MN]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[MN]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[MN]*jpg -v
-echo "Commiting files that start with [OP]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[OP]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[OP]*jpg -v
-echo "Commiting files that start with [QR]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[QR]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[QR]*jpg -v
-echo "Commiting files that start with [ST]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[ST]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[ST]*jpg -v
-echo "Commiting files that start with [UVWX]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[UVWX]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[UVWX]*jpg -v
-echo "Commiting files that start with [YZ]..."
-git commit -m "More Updates"  Linear/Charts_With_Numbers/[YZ]*jpg -v
-git commit -m "More Updates"  Linear/Charts_Without_Numbers/[YZ]*jpg -v
+
+for chrs in $alphabets 
+do 
+  echo "Adding the files that start with ==> [$chrs]*"
+  git  add Linear/Charts_With_Numbers/[$chrs]*jpg -v
+  git  add Linear/Charts_Without_Numbers/[$chrs]*jpg -v
+done
+
+for chrs in $alphabets 
+do 
+  echo ""
+  echo "=============================="
+  echo "Committing the files that start with ==> [$chrs]*"
+  echo "=============================="
+  echo ""
+  ## sleep 2
+  git commit -m "More Updates"  Linear/Charts_With_Numbers/[$chrs]*jpg -v
+  git commit -m "More Updates"  Linear/Charts_Without_Numbers/[$chrs]*jpg -v
+done
+
